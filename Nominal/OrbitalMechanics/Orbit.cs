69043c18ed711383059eb855cd0b99a028108b16
unknown <rettenbacher.phil@gmail.com>
2018-03-15 22:34:00 +0100

    Started doing some Orbit stuff

---
+++ Nominal/OrbitalMechanics/Orbit.cs
@@ -0,0 +1,105 @@
+﻿using Nominal.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nominal.OrbitalMechanics
+{
+    public class Orbit
+    {
+        public double trueAnomaly
+        {
+            get
+            {
+                return _trueAnomaly;
+            }
+        }
+        private double _trueAnomaly;
+
+        public double semiMajorAxis
+        {
+            get
+            {
+                return _semiMajorAxis;
+            }
+        }
+        private double _semiMajorAxis;
+
+        public double eccentricity
+        {
+            get
+            {
+                return _eccentricity;
+            }
+        }
+        private double _eccentricity;
+
+        public double argumentOfPeriapsis
+        {
+            get
+            {
+                return _argumentOfPeriapsis;
+            }
+        }
+        private double _argumentOfPeriapsis;
+
+        private double meanAnomaly;
+        private double gravParameter;
+
+        public Orbit(double semiMajorAxis, double eccentricity, double argumentOfPeriapsis, double gravParameter)
+        {
+            _semiMajorAxis = semiMajorAxis;
+            _eccentricity = eccentricity;
+            _argumentOfPeriapsis = argumentOfPeriapsis;
+            this.gravParameter = gravParameter;
+        }
+
+        public double periapsis
+        {
+            get
+            {
+                return _semiMajorAxis * (1 - _eccentricity);
+            }
+        }
+        public double apoapsis
+        {
+            get
+            {
+                return _semiMajorAxis * (1 + _eccentricity);
+            }
+        }
+        public double radius
+        {
+            get
+            {
+                return _semiMajorAxis * (1 - System.Math.Pow(_eccentricity, 2)) / (1 + _eccentricity * System.Math.Cos(_trueAnomaly));
+            }
+        }
+        public DVector2 positionVector
+        {
+            get
+            {
+                //TODO
+                return DVector2.zero;
+            }
+        }
+        public double velocity
+        {
+            get
+            {
+                //TODO
+                return 0;
+            }
+        }
+        public DVector2 velocityVector
+        {
+            get
+            {
+                //TODO
+                return DVector2.zero;
+            }
+        }
+    }
+}
---
+++ Nominal/OrbitalMechanics/OrbitMath.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nominal.OrbitalMechanics
+{
+    public static class OrbitMath
+    {
+        /// <summary>
+        /// Converts True Anomaly to Mean Anomaly
+        /// </summary>
+        public static double ConvertTrueToMeanElliptic(double trueAnomaly, double eccentricity)
+        {
+            double E = System.Math.Acos((eccentricity + System.Math.Cos(trueAnomaly)) / (1 + eccentricity * System.Math.Cos(trueAnomaly)));
+            return E - eccentricity * System.Math.Sin(E);
+        }
+        /// <summary>
+        /// Converts Mean Anomaly to True Anomaly
+        /// </summary>
+        public static double ConvertMeanToTrueElliptic(double meanAnomaly, double eccentricity)
+        {
+            //spooky scary math stuff
+            double deltaE = 0;
+            double E = 0;
+
+            //Aproximation of starting values
+            double El = meanAnomaly;
+            if (eccentricity < 0.7)
+            {
+                El = System.Math.Pow(6 * meanAnomaly, 1 / 3);
+            }
+
+            //This is just old code, but it works
+            int maxCount = 1000;
+            for (int i = 0; i < maxCount; i++)
+            {
+                E = meanAnomaly + eccentricity * System.Math.Sin(El);
+                deltaE = E - El;
+                if (deltaE == 0)
+                    break;
+                El = E;
+
+            }
+            double v = System.Math.Acos((System.Math.Cos(E) - eccentricity) / (1 - eccentricity * System.Math.Cos(E)));
+            if (E > System.Math.PI)
+                v = 2 * System.Math.PI - v;
+            return v;
+        }
+
+    }
+}
